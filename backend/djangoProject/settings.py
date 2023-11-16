76bfe37101188809cf7275eff57d80c7bb8e00f1
Łukasz Białas <lukasz.bialas@student.pk.edu.pl>
2023-11-16 20:15:29 +0100

    swagger and token

--- backend/djangoProject/settings.py
+++ backend/djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+    'drf_yasg',
+    'app',
+    'authentification'
 ]
 
 MIDDLEWARE = [
@@ -52,6 +58,19 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+      ],
+}
+
+SIMPLE_JWT = {
+     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+     'ROTATE_REFRESH_TOKENS': True,
+     'BLACKLIST_AFTER_ROTATION': True
+}
 
 ROOT_URLCONF = 'djangoProject.urls'
 
--- backend/djangoProject/urls.py
+++ backend/djangoProject/urls.py
@@ -24,13 +24,42 @@ Including another URLconf
 # ]
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 from .views import *
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from rest_framework_simplejwt import views as jwt_views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API documentation for your project",
+        # Możesz dodać więcej metadanych zgodnie z potrzebą
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('app/', include('app.urls')),
+
     path('api_test/', api_test, name='api_test'),
     path('login/', login_page, name='login_page'),
-    path('index/', TemplateView.as_view(template_name="../../frontend/public/index.html"), name='index')
+    # path('register/', register_page, name='register_page'),
+    path('index/', TemplateView.as_view(template_name="../../frontend/public/index.html"), name='index'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', include('authentification.urls')),
+    #path('logout/', views.LogoutView.as_view(), name ='logout'),
+    path('token/',
+          jwt_views.TokenObtainPairView.as_view(),
+          name ='token_obtain_pair'),
+     path('token/refresh/',
+          jwt_views.TokenRefreshView.as_view(),
+          name ='token_refresh')
 ]
